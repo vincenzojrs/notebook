3fc963732cdc3669769ec5243c4ad04cbf87bc50
Vincenzo <92443685+vincenzojrs@users.noreply.github.com>
2024-03-03 11:27:38 +0100

    Update ufoapp_OOP.py
    adding "preprocessing data" so that when "24:00" occurs in datetime column of the dataframe, it changes to "00:00" +1 day

--- streamlit/ufoapp_OOP.py
+++ streamlit/ufoapp_OOP.py
@@ -6,24 +6,34 @@ np.random.seed(42)
 
 class NUFORCReportApp:
     def __init__(self):
-        self.data = self.load_data(100)
-        
+        self.data = self.load_data(500)
+        self.data = self.preprocess_data(self.data)        
+    
     def load_data(self, nrows):
         data = pd.read_csv("complete.csv", nrows=nrows)
-        data["datetime"] = pd.to_datetime(data["datetime"])
         return data
-
+    
+    def preprocess_data(self, data):
+        """
+        Convert strings in the date time column so that when "24:00" appears, change it in "00:00" +1 day
+        """
+        twenty_fours = data['datetime'].str[-5:] == '24:00'
+        data.loc[twenty_fours, 'datetime'] = data['datetime'].str[:-5] + '00:00'
+        data['datetime'] = pd.to_datetime(data['datetime'], format='%d/%m/%Y %H:%M')
+        data.loc[twenty_fours, 'datetime'] += pd.DateOffset(1)
+        return data
+        
     def display_raw_data(self):
         st.subheader("Raw Data")
         st.dataframe(self.data, use_container_width=True)
-
+        
     def display_sighting_report_by_hour(self):
         st.subheader("Number of sighting report by hour")
         hist_values = np.histogram(self.data["datetime"].dt.hour, bins=24, range=(0, 24))[0]
         st.bar_chart(hist_values)
         
     def display_map(self):
-        st.subheader("Map of sighting")
+        st.subheader("Map of sightings")
         st.map(self.data)
 
     def run(self):
