493bef4034848ad73b8b6fa32e370fc7b2d59c14
Vincenzo <92443685+vincenzojrs@users.noreply.github.com>
2024-03-03 11:40:34 +0100

    Update ufoapp_OOP.py
    added a global filter "hour_to_filter" both to the dataframe and the map

--- streamlit/ufoapp_OOP.py
+++ streamlit/ufoapp_OOP.py
@@ -32,17 +32,26 @@ class NUFORCReportApp:
         hist_values = np.histogram(self.data["datetime"].dt.hour, bins=24, range=(0, 24))[0]
         st.bar_chart(hist_values)
         
-    def display_map(self):
-        st.subheader("Map of sightings")
-        st.map(self.data)
-
+    def display_map_filtered(self, hour_to_filter):
+        st.subheader("Map of sightings at {}:00".format(hour_to_filter))
+        st.map(self.data_filtered)
+        
+    def display_raw_data_filtered(self, hour_to_filter):
+        st.subheader("Filtered data by {}:00 ".format(hour_to_filter))
+        st.dataframe(self.data_filtered, use_container_width=True)
+    
     def run(self):
         st.title("NUFORC Report App")
         st.write("Let's try to build an app regarding UFO seeing")
         
         self.display_raw_data()
         self.display_sighting_report_by_hour()
-        self.display_map()
+        
+        hour_to_filter = st.slider('hour', 0, 23, 17)
+        self.data_filtered = self.data[self.data["datetime"].dt.hour == hour_to_filter]
+        
+        self.display_map_filtered(hour_to_filter)
+        self.display_raw_data_filtered(hour_to_filter)
 
 if __name__ == "__main__":
     app = NUFORCReportApp()
