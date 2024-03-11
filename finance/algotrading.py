6d6271e7d70c17f5183104fb619101667faf6e53
Vincenzo <92443685+vincenzojrs@users.noreply.github.com>
2024-03-11 14:28:49 +0100

    Update algotrading.py
    added monthly aggregation, ranking and filtering

--- finance/algotrading.py
+++ finance/algotrading.py
@@ -1,4 +1,7 @@
+
 # Inspired by  Algorithmic Trading – Machine Learning & Quant Strategies Course with Python by freeCodeCamp.org
+!pip install pandas-ta
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -82,6 +85,36 @@ class AlgoTradingStrategy:
     df['dollar_volume'] = (df['adj close'] * df['volume']) / 1e6
     return df
 
+  def monthly_aggregation(self, df):
+    """ Get the average monthly liquidity and monthly indicators, taking the last value per month """
+
+    # Define the columns you want to keep, which are the indicators
+    last_cols = [c for c in df.columns.unique(0) if c not in ['dollar_volume', 'volume', 'open', 'high', 'low', 'close']]
+
+    avg_dollar_volume = df['dollar_volume'].unstack('ticker').resample('M').mean().stack('ticker').to_frame('dollar_volume')
+
+    # Keep the last value per each month, per each ticker for selected columns
+    last_values = df.unstack()[last_cols].resample('M').last().stack('ticker')
+
+    # Concatenate the calculated values
+    df = pd.concat([avg_dollar_volume, last_values], axis=1).dropna()
+
+    return df
+
+  def ranking_and_filtering(self, df):
+    """ Calculate the 5-year rolling average of dollar volume for each stock and filter in the first 150 most liquid companies """
+
+    #  Calculate the dollar volume 5 years * 12 months rolling average
+    df['dollar_volume_5'] = (df['dollar_volume'].unstack('ticker').rolling(5*12).mean().stack())
+
+    # Rank the stock based on the 5-year rolling average
+    df['dollar_volume_5_rank'] = (df.groupby('date')['dollar_volume_5'].rank(ascending =False))
+
+    # Filter the first 150 most liquid companies
+    df = df[df['dollar_volume_5_rank'] < 150].drop(['dollar_volume', 'dollar_volume_5_rank'], axis=1)
+
+    return df
+
 strategy = AlgoTradingStrategy('2023-09-27')
 
 df = strategy.download_dataframe()
@@ -89,3 +122,7 @@ df = strategy.download_dataframe()
 df_fixed = strategy.fixed_dataframe(df)
 
 strategy.dollar_volume(df_fixed)
+
+df_fixed = strategy.monthly_aggregation(df_fixed)
+
+df_fixed = strategy.ranking_and_filtering(df_fixed)
